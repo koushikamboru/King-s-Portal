89564dd1a75c26f8338c4f2411b6db7639e40bc3
koushikamboru <koushic1980@gmail.com>
2024-02-23 13:13:31 +0530

    col

--- JS/index.js
+++ JS/index.js
@@ -7,23 +7,25 @@ function currentSlide(n) {
 }
 
 function showSlides() {
-    let i;
     const slides = document.querySelectorAll('.hero_img');
+    const infos = document.querySelectorAll('.hero_info_cont');
     const dots = document.querySelectorAll('.dot');
     
-    // Hide all slides and remove active class from dots
-    for (i = 0; i < slides.length; i++) {
+    // Hide all slides, infos, and remove active class from dots
+    for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = 'none';
+        infos[i].style.display = 'none';
         dots[i].classList.remove('active');
     }
     
-    // Show current slide and add active class to corresponding dot
+    // Show current slide, info, and add active class to corresponding dot
     slides[slideIndex].style.display = 'block';
+    infos[slideIndex].style.display = 'block';
     dots[slideIndex].classList.add('active');
     
     slideIndex++;
     if (slideIndex >= slides.length) {
         slideIndex = 0;
     }
-    setTimeout(showSlides, 6500); // Change slide every 6 seconds
+    setTimeout(showSlides, 6500); // Change slide every 6.5 seconds
 }
